22975ab91f32541d12f0e717a45f1ea232a7584a
jcarres-mdsol <jcarres@mdsol.com>
2019-01-08 13:18:53 -0800

    set env variables properly

--- run.sh
+++ run.sh
@@ -17,6 +17,9 @@ java -jar swagger2markup-cli-1.3.3.jar convert \
 #    -c ${dir}/config.properties \
     >$(tty)
 
+echo "adoc document generated, rendering beautifully into PDF..."
+export PATH=$PATH:${dir}/gems/bin
+export GEM_PATH=$GEM_PATH:${dir}/gems
 # Convert AsciiDoc to PDF (output to stdout).
-ruby gems/bin/asciidoctor-pdf -a toc=left -a toclevels=3 --out-file=openapi.pdf openapi.adoc
+asciidoctor-pdf -a toc=left -a toclevels=3 --out-file=openapi.pdf openapi.adoc
 
