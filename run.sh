3aef538ad7e8fc0cae0c94de7dc27a6680a17309
jcarres-mdsol <jcarres@mdsol.com>
2019-01-08 11:30:23 -0800

    add run file

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+source=$1
+
+if [ -z "$1" ]
+then
+    echo "Pass the path to the openapi.yaml file as a parameter to this script"
+    exit -1
+fi
+dir=$(  pwd -P )
+
+# Generate docs in AsciiDoc format > `tmp/index.adoc`.
+java -jar ./build/libs/swagger2markup-cli-1.3.3.jar convert \
+    -i ${source} \
+    -f ${dir}/openapi \
+#    -c ${dir}/config.properties \
+    >$(tty)
+
+# Convert AsciiDoc to PDF (output to stdout).
+asciidoctor-pdf -a toc=left -a toclevels=3 --out-file=openapi.pdf openapi.adoc
+
